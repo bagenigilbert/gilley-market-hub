a902ef3027ce5f7b640592a5c860bdc959a2f3cc
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-11 14:59:42 +0300

    added login and profile for user links

--- gilley-market/src/App.js
+++ gilley-market/src/App.js
@@ -8,20 +8,26 @@ const Header = () => {
     return(
         //inside header component
         <header className="header"> {/* This is like building the top part of the website */}
-        <div className="logo">
-             {/* This is the name of the website */}
-        <Link to="/">GILLEY MARKET HUB</Link>
-        </div>
-        <nav className="nav-links">
+            <div className="logo">
+                {/* This is the name of the website */}
+            <Link to="/">GILLEY MARKET HUB</Link>
+            </div>
+            <nav className="nav-links">
             <ul>
                  {/* These are like the buttons to different places */}
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/products">Products</Link></li>
-          <li><Link to="/cart">Cart</Link></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/products">Products</Link></li>
+                <li><Link to="/cart">Cart</Link></li>
             </ul>
+            </nav>
+            <div className="user-links">
+                {/* These are buttons for users */}
+                <Link to="/login">Login</Link>
+                <Link to="/profile">Profile</Link>
+            </div>
+    </header>
 
-        </nav>
-
-        </header>
     );
 }
+// Export the Header component to use in other parts
+export default Header;

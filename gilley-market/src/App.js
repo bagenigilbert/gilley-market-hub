308db4fdc42614e0e8d7101c8c64a99aa280fbaa
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-11 14:45:30 +0300

    added components for the project

---
+++ gilley-market/src/App.js
@@ -0,0 +1,11 @@
+//import React and tools we need
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css"; // Import the styles for the header
+
+//create the header component
+const Header = () => {
+    return(
+        //inside header component
+    );
+}
---
+++ gilley-market/src/components/header/Header.js
@@ -0,0 +1,12 @@
+// // import React and tools needed 
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import ".Header.css";// Import the styles for the header
+
+//    // Create the Header component
+// 	const Header = () =>{
+// 		return(
+			
+// 					);
+// 			}
+// 			export default Header;
